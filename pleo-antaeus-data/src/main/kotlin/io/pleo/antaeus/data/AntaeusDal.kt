e3b331e9dadcb14f97d402ecf5c2747341f9dc0f
Eylül Dicle Yurdakul <eylul@copyrightagent.com>
2019-08-23 14:37:09 +0200

    Add function to fetch invoices with a given status to InvoiceService and database

--- pleo-antaeus-data/src/main/kotlin/io/pleo/antaeus/data/AntaeusDal.kt
+++ pleo-antaeus-data/src/main/kotlin/io/pleo/antaeus/data/AntaeusDal.kt
@@ -38,6 +38,14 @@ class AntaeusDal(private val db: Database) {
         }
     }
 
+    fun fetchInvoicesWithStatus(status: InvoiceStatus): List<Invoice> {
+        return transaction(db) {
+            InvoiceTable
+                .select { InvoiceTable.status.eq(status.toString()) }
+                .map { it.toInvoice() }
+        }
+    }
+
     fun createInvoice(amount: Money, customer: Customer, status: InvoiceStatus = InvoiceStatus.PENDING): Invoice? {
         val id = transaction(db) {
             // Insert the invoice and returns its new id.
