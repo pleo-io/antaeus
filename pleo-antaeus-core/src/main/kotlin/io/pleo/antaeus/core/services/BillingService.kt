b4e122dd3a219243b48a65d49cf7232ec97c57e5
Daniel Felipe Klotz <danielfelipe.klotz@gi-de.com>
2020-12-21 23:43:45 +0100

    1. Created methods for invoice billing in BillingService.kt 2. Added the new ERROR status in InvoiceStatus.kt 3. Created two new methods for fetching and updating invoices in InvoiceService.kt

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,9 +1,68 @@
 package io.pleo.antaeus.core.services
 
+import io.pleo.antaeus.core.exceptions.CurrencyMismatchException
+import io.pleo.antaeus.core.exceptions.CustomerNotFoundException
+import io.pleo.antaeus.core.exceptions.InvoiceNotFoundException
+import io.pleo.antaeus.core.exceptions.NetworkException
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
 
 class BillingService(
-    private val paymentProvider: PaymentProvider
+        private val paymentProvider: PaymentProvider,
+        private val invoiceService: InvoiceService
 ) {
-// TODO - Add code e.g. here
+
+    fun chargeInvoices(invoiceStatus: InvoiceStatus): Boolean {
+        when(invoiceStatus) {
+            InvoiceStatus.PENDING -> handlePendingInvoices()
+            //Todo: In case we have more invoice status(eg. Error, Partially Paid, Dispute), other functions can be added
+        }
+        logger.info { "The payment of the selected invoices has been processed" }
+        return true
+    }
+
+    private fun handlePendingInvoices() {
+        val pendingInvoices: List<Invoice> = invoiceService.fetchInvoicesWithStatus(InvoiceStatus.PENDING)
+        for (pendingInvoice in pendingInvoices) {
+            chargePendingInvoice(pendingInvoice)
+        }
+    }
+
+    fun chargePendingInvoice(invoice: Invoice) {
+        try {
+
+            if (paymentProvider.charge(invoice)) {
+                invoiceService.updateInvoiceStatus(invoice.id, InvoiceStatus.PAID)
+            } else {
+                //Todo: new InvoiceStatus could be set accordingly to the error here
+                invoiceService.updateInvoiceStatus(invoice.id, InvoiceStatus.ERROR)
+                logger.info { "Failed to charge the invoice ${invoice.id} from customer ${invoice.customerId}." }
+            }
+
+        } catch (e: CurrencyMismatchException) {
+            //Todo: new InvoiceStatus could be set accordingly to the Exception here
+            invoiceService.updateInvoiceStatus(invoice.id, InvoiceStatus.ERROR)
+            logger.error(e) { "The currency is not matching the invoice ${invoice.id}." }
+            throw e
+        } catch (e: CustomerNotFoundException) {
+            //Todo: new InvoiceStatus could be set accordingly to the Exception here
+            invoiceService.updateInvoiceStatus(invoice.id, InvoiceStatus.ERROR)
+            logger.error(e) { "The customerId ${invoice.customerId} specified in the invoice was not found ${invoice.id}." }
+            throw e
+        } catch (e: InvoiceNotFoundException) {
+            //Todo: new InvoiceStatus could be set accordingly to the Exception here
+            invoiceService.updateInvoiceStatus(invoice.id, InvoiceStatus.ERROR)
+            logger.error(e) { "The Invoice ${invoice.id} could not be found." }
+            throw e
+        } catch (e: NetworkException) {
+            //Todo: new InvoiceStatus could be set accordingly to the Exception here
+            invoiceService.updateInvoiceStatus(invoice.id, InvoiceStatus.ERROR)
+            logger.error(e) { "A network issue was found." }
+            throw e
+        }
+    }
 }
--- pleo-antaeus-data/src/main/kotlin/io/pleo/antaeus/data/AntaeusDal.kt
+++ pleo-antaeus-data/src/main/kotlin/io/pleo/antaeus/data/AntaeusDal.kt
@@ -12,10 +12,7 @@ import io.pleo.antaeus.models.Customer
 import io.pleo.antaeus.models.Invoice
 import io.pleo.antaeus.models.InvoiceStatus
 import io.pleo.antaeus.models.Money
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.select
-import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class AntaeusDal(private val db: Database) {
@@ -38,6 +35,23 @@ class AntaeusDal(private val db: Database) {
         }
     }
 
+    fun fetchInvoicesWithStatus(status: InvoiceStatus): List<Invoice> {
+        return transaction(db) {
+            InvoiceTable
+                .select { InvoiceTable.status.eq(status.toString()) }
+                .map { it.toInvoice() }
+        }
+    }
+
+    fun updateInvoiceStatus(invoiceId: Int, invoiceStatus: InvoiceStatus) {
+        transaction(db) {
+            InvoiceTable
+                .update({ InvoiceTable.id eq invoiceId}) {
+                    it[this.status] = invoiceStatus.toString()
+                }
+        }
+    }
+
     fun createInvoice(amount: Money, customer: Customer, status: InvoiceStatus = InvoiceStatus.PENDING): Invoice? {
         val id = transaction(db) {
             // Insert the invoice and returns its new id.
@@ -48,7 +62,7 @@ class AntaeusDal(private val db: Database) {
                     it[this.status] = status.toString()
                     it[this.customerId] = customer.id
                 } get InvoiceTable.id
-        }
+        } 
 
         return fetchInvoice(id)
     }
