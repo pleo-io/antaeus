cd16a60a9d561a427b274c798cbf390c9d869ec2
lnury <lnury@demo.fr>
2021-03-05 10:07:24 +0100

    Update method name

--- pleo-antaeus-data/src/main/kotlin/io/pleo/antaeus/data/AntaeusDal.kt
+++ pleo-antaeus-data/src/main/kotlin/io/pleo/antaeus/data/AntaeusDal.kt
@@ -12,10 +12,7 @@ import io.pleo.antaeus.models.Customer
 import io.pleo.antaeus.models.Invoice
 import io.pleo.antaeus.models.InvoiceStatus
 import io.pleo.antaeus.models.Money
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.select
-import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class AntaeusDal(private val db: Database) {
@@ -30,7 +27,7 @@ class AntaeusDal(private val db: Database) {
         }
     }
 
-    fun fetchPendingInvoice(): List<Invoice> {
+    fun fetchPendingInvoices(): List<Invoice> {
         return transaction(db) {
             InvoiceTable
                 .select { InvoiceTable.status.eq(InvoiceStatus.PENDING.name) }
