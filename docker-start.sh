a501893102e99d333a4c94da01a41894779cfe10
lnury <lnury@demo.fr>
2021-03-05 11:36:07 +0100

    Fix script : move comment

--- docker-start.sh
+++ docker-start.sh
@@ -5,12 +5,11 @@ set -x
 # Create a new image version with latest code changes.
 docker build . --tag pleo-antaeus
 
-# Build the code.
+# Build the code. The volume is only there so incremental builds are way faster
 docker run \
   --publish 7000:7000 \
   --rm \
   --interactive \
   --tty \
-  # This volume is only there so incremental builds are way faster
   --volume pleo-antaeus-build-cache:/root/.gradle \
   pleo-antaeus
