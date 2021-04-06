f8c3b9fb4616a3bc1560d56c2d86bbe94eaae38d
lnury <lnury@demo.fr>
2021-04-06 11:14:28 +0200

    Use a proper logger and simplify code

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -5,37 +5,38 @@ import io.pleo.antaeus.core.exceptions.CustomerNotFoundException
 import io.pleo.antaeus.core.exceptions.NetworkException
 import io.pleo.antaeus.core.external.PaymentProvider
 import io.pleo.antaeus.models.Invoice
+import mu.KotlinLogging
 
 class BillingService(
     private val paymentProvider: PaymentProvider,
     private val invoiceService: InvoiceService
 ) {
-    // FIXME use a proper logger framework instead of println
+    private val log = KotlinLogging.logger {}
+
     /*
      * Charges all the pending invoices and return the list of paid invoices
      */
-    fun chargePendingInvoices(): List<Invoice> {
-        val invoices = invoiceService.fetchPendingInvoices()
-        val paidInvoices: List<Invoice> = mutableListOf()
-        mutableListOf<Invoice>()
-        for (i in invoices) {
+    fun chargePendingInvoices() {
+        invoiceService.fetchPendingInvoices().forEach{
             try {
-                if (paymentProvider.charge(i)) {
-                    invoiceService.setInvoicePaid(i.id)
-                    println("INFO - Invoice " + i.id + " successfully paid!")
+                if (paymentProvider.charge(it)) {
+                    invoiceService.setInvoicePaid(it.id)
+                    log.info("Invoice ${it.id} successfully paid!")
+                } else {
+                    log.warn { "Invoice ${it.id} cannot be paid: customer ${it.customerId} account balance did not allow the charge" }
+                    // TODO notify the business: message queue, db record, ...
                 }
             } catch (e: CustomerNotFoundException) {
-                println("WARNING - Invoice " + i.id + " cannot be paid because customer " + i.customerId + " could not be found")
-                // TODO notify admin of this data inconsistency (using a message queue for example)
+                log.error("Invoice ${it.id} cannot be paid: customer ${it.customerId} not found")
+                // TODO notify admin of this data inconsistency: message queue, log alert, ...
             } catch (e: CurrencyMismatchException) {
-                println("WARNING - Invoice " + i.id + " cannot be paid because currency doesn't match the one of the customer " + i.customerId)
-                // TODO notify business that actions must be taken to fix the problem (message queue)
+                log.error("Invoice ${it.id} cannot be paid: currency mismatch with customer ${it.customerId}")
+                // TODO notify business that actions must be taken to fix the problem: message queue, db record, ...
             } catch (e: NetworkException) {
-                println("WARNING - Invoice " + i.id + " cannot be paid because of a network issue")
+                log.error("Invoice ${it.id} cannot be paid: network issue")
                 // TODO add this invoice in a queue so the billing can be retried later
             }
         }
-        return paidInvoices
     }
 
 }
