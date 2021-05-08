18a7bf4aab656b9e3d3af788c7415cc3eecf8d2f
Andriy Godovanets <andriy.g.work@gmail.com>
2021-05-08 15:59:03 +0300

    AN-5: refactoring setupInitialData - parameterized with customersNum and invoicesPerCustomerNum

--- pleo-antaeus-data/src/main/kotlin/io/pleo/antaeus/data/utils.kt
+++ pleo-antaeus-data/src/main/kotlin/io/pleo/antaeus/data/utils.kt
@@ -18,15 +18,15 @@ fun nextMonthDate(date: Date): Date {
 }
 
 // This will create all schemas and setup initial data
-suspend fun setupInitialData(dal: AntaeusDal)  {
-    val customers = (1..100).mapNotNull {
+suspend fun setupInitialData(dal: AntaeusDal, customersNum: Int = 100, invoicesPerCustomerNum: Int = 10)  {
+    val customers = (1..customersNum).mapNotNull {
         dal.createCustomer(
                 currency = Currency.values()[Random.nextInt(0, Currency.values().size)]
         )
     }
 
     customers.forEach { customer ->
-        (1..10).forEach {
+        (1..invoicesPerCustomerNum).forEach {
             dal.createInvoice(
                     amount = Money(
                             value = BigDecimal(Random.nextDouble(10.0, 500.0)),
