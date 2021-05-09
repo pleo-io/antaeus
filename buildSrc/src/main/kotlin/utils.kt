e08f51a5b1c53156630a78b258a6ebead31e6b3e
Andriy Godovanets <andriy.g.work@gmail.com>
2021-05-09 23:06:07 +0300

    AN-6: added kjob scheduler; bumped kotlin version to 1.4.10; kotlin-coroutines version - 1.4.0

--- buildSrc/src/main/kotlin/utils.kt
+++ buildSrc/src/main/kotlin/utils.kt
@@ -4,8 +4,9 @@ import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.kotlin
 
 const val junitVersion = "5.7.0"
-const val kotlinCoroutinesVersion = "1.3.5"
+const val kotlinCoroutinesVersion = "1.4.0"
 const val h2Version = "1.4.199"
+const val kjobVersion = "0.2.0"
 
 /**
  * Configures the current project as a Kotlin project by adding the Kotlin `stdlib` as a dependency.
@@ -14,11 +15,18 @@ fun Project.kotlinProject() {
     dependencies {
         // Kotlin libs
         "implementation"(kotlin("stdlib"))
+        "implementation"(kotlin("reflect"))
+
         "implementation"("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
 
         // Logging
         "implementation"("org.slf4j:slf4j-simple:1.7.30")
         "implementation"("io.github.microutils:kotlin-logging:1.7.8")
+
+        // Scheduler
+        "implementation"("it.justwrote:kjob-core:$kjobVersion")
+        "implementation"("it.justwrote:kjob-kron:$kjobVersion")
+        "implementation"("it.justwrote:kjob-inmem:$kjobVersion")
 
         // Mockk
         "testImplementation"("io.mockk:mockk:1.9.3")
