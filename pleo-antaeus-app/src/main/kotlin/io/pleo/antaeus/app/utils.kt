3f950a32f6d856da334a575b3e5100c5c4718ac7
Andriy Godovanets <andriy.g.work@gmail.com>
2021-05-10 14:51:43 +0300

    AN-7: extended payment provider mock to throw CustomerNotFoundException and CurrencyMismatchException

--- pleo-antaeus-app/src/main/kotlin/io/pleo/antaeus/app/utils.kt
+++ pleo-antaeus-app/src/main/kotlin/io/pleo/antaeus/app/utils.kt
@@ -1,7 +1,10 @@
 package io.pleo.antaeus.app
 
+import io.pleo.antaeus.core.exceptions.CurrencyMismatchException
+import io.pleo.antaeus.core.exceptions.CustomerNotFoundException
 import io.pleo.antaeus.core.exceptions.NetworkException
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.models.Currency
 import io.pleo.antaeus.models.Invoice
 import kotlin.random.Random
 
@@ -11,6 +14,12 @@ internal fun getPaymentProvider(): PaymentProvider {
         override fun charge(invoice: Invoice): Boolean {
             if (Random.nextInt(100) < 5) {
                 throw NetworkException()
+            }
+            if (invoice.customerId == 1) {
+                throw CustomerNotFoundException(invoice.customerId)
+            }
+            if (invoice.amount.currency == Currency.GBP) {
+                throw CurrencyMismatchException(invoiceId = invoice.id, customerId = invoice.customerId)
             }
             return Random.nextBoolean()
         }
